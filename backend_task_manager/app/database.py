cc424e9fae4a64a696e177f088a127bc2a7e25bf
himanshijindgar <46708657+himanshijindgar@users.noreply.github.com>
2026-01-21 21:50:30 +0530

    Update database.py

--- backend_task_manager/app/database.py
+++ backend_task_manager/app/database.py
@@ -1,14 +1,13 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
+from .settings import settings
 
 connect_args = {}
-if DATABASE_URL.startswith("sqlite"):
+if settings.DATABASE_URL.startswith("sqlite"):
     connect_args = {"check_same_thread": False}
 
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(settings.DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
